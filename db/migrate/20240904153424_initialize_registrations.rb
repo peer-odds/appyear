93ae22c57f6c44ba05833273361d097c17143b7f
peer-odds <peer.tui@odds.team>
2024-09-04 22:38:26 +0700

    add model attibute for register page

--- db/migrate/20240904153424_initialize_registrations.rb
+++ db/migrate/20240904153424_initialize_registrations.rb
@@ -1,4 +1,14 @@
 class InitializeRegistrations < ActiveRecord::Migration[7.1]
   def change
+    create_table :registrations do |t|
+      t.string :first_name
+      t.string :last_name
+      t.date :dob
+      t.string :gender
+      t.string :email
+      t.string :phone
+      t.string :subject
+      t.timestamps
+    end
   end
 end
