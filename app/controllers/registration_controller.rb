f6b7e42607da1d59b758ef81f728fcfe58e1bdb3
peer-odds <peer.tui@odds.team>
2024-09-04 22:47:29 +0700

    create registrations controller and define param require to create new registrations , create method for create new registrations

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,14 @@
+class RegistrationsController < ApplicationController
+  private 
+  def create 
+    @registration = Registration.create(registration_params)
+    if @registration.save
+      render json: @registration
+    else
+      render json: @registration.errors, status: :unprocessable_entity
+    end
+  end
+  def registration_params
+    params.require(:registration).permit(:first_name, :last_name, :dob, :gender, :email, :phone, :subject)
+  end
+end
